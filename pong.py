e88464a6463d2d30c728bffa69b90fc03f157514
Jorge Eduardo Ardila <jeam@tuta.io>
2021-05-16 12:12:04 -0500

    juego pong completo

--- pong.py
+++ pong.py
@@ -7,24 +7,24 @@ pg.bgcolor("lime")
 pg.setup(width=800, height=600)
 pg.tracer(0)
 
-# raqueta 1
-raq_1 = turtle.Turtle()
-raq_1.speed(0)
-raq_1.shape("square")
-raq_1.color("white")
-raq_1.shapesize(stretch_wid=5, stretch_len=1)
-raq_1.goto(350, 0)
-raq_1.penup()
+# raqueta Derecha
+raq_d = turtle.Turtle()
+raq_d.speed(0)
+raq_d.shape("square")
+raq_d.color("white")
+raq_d.shapesize(stretch_wid=5, stretch_len=1)
+raq_d.goto(350, 0)
+raq_d.penup()
 
 
-# raqueta 2
-raq_2 = turtle.Turtle()
-raq_2.speed(0)
-raq_2.shape("square")
-raq_2.color("white")
-raq_2.shapesize(stretch_wid=5, stretch_len=1)
-raq_2.goto(-350, 0)
-raq_2.penup()
+# raqueta Izquierda
+raq_i = turtle.Turtle()
+raq_i.speed(0)
+raq_i.shape("square")
+raq_i.color("white")
+raq_i.shapesize(stretch_wid=5, stretch_len=1)
+raq_i.goto(-350, 0)
+raq_i.penup()
 
 # bola
 bola = turtle.Turtle()
@@ -41,39 +41,39 @@ pt = turtle.Turtle()
 pt.speed(0)
 pt.color('white')
 pt.goto(0, 260)
-pt.write("Jugador A: 0  Jugador B: 0", align='center', font=('Courier', 24, 'bold'))
+pt.write("Jugador Derecha: 0  Jugador Izquierda: 0", align='center', font=('Courier', 24, 'bold'))
 pt.hideturtle()
 
 # puntajes / scores
-score_1 = 0
-score_2 = 0
+score_d = 0
+score_i = 0
 
-def raq_1_up():
-    y = raq_1.ycor()
+def raq_i_up():
+    y = raq_i.ycor()
     y += 20
     raq_1.sety(y)
 
-def raq_1_down():
-    y = raq_1.ycor()
-    y += -20
-    raq_1.sety(y)
+def raq_i_down():
+    y = raq_i.ycor()
+    y -= -20
+    raq_i.sety(y)
 
-def raq_2_up():
-    y = raq_2.ycor()
+def raq_d_up():
+    y = raq_d.ycor()
     y += 20
-    raq_2.sety(y)
+    raq_d.sety(y)
 
-def raq_2_down():
-    y = raq_2.ycor()
-    y += -20
-    raq_2.sety(y)
+def raq_d_down():
+    y = raq_d.ycor()
+    y -= -20
+    raq_d.sety(y)
 
 pg.listen()
 
-pg.onkey(raq_1_up, 'Up')
-pg.onkey(raq_1_down, 'Down')
-pg.onkeypress(raq_2_up, 'w')
-pg.onkey(raq_2_down, 's')
+pg.onkey(raq_d_up, 'Up')
+pg.onkey(raq_d_down, 'Down')
+pg.onkeypress(raq_i_up, 'w')
+pg.onkey(raq_i_down, 's')
 
 while True:
     pg.update()
@@ -88,24 +88,23 @@ while True:
     if bola.xcor() > 390:
         bola.goto(0, 0)
         bola.dx *= -1
-        score_1 += 1
+        score_d += 1
         pt.clear()
-        pt.write("Jugador A: {} Juagdor B: {}".format(score_1, score_2), align='center', font=('Courier', 24, 'bold'))
+        pt.write("Jugador A: {} Juagdor B: {}".format(score_d, score_i), align='center', font=('Courier', 24, 'bold'))
 
     if bola.xcor() < -390:
         bola.goto(0, 0)
         bola.dx *= -1
-        score_2 += 1
+        score_i += 1
         pt.clear()
-        pt.write("Jugador A: {} Juagdor B: {}".format(score_1, score_2), align='center', font=('Courier', 24, 'bold'))
+        pt.write("Jugador A: {} Juagdor B: {}".format(score_d, score_i), align='center', font=('Courier', 24, 'bold'))
 
     # golpe a la bola: colision
-    if (bola.xcor() > 340 and bola.xcor() < 350)  and (bola.ycor() < raq_2.ycor() + 60 and bola.ycor() > raq_2.ycor() - 60 ):
+    if (bola.xcor() > 340 and bola.xcor() < 350)  and (bola.ycor() < raq_i.ycor() + 60 and bola.ycor() > raq_i.ycor() - 60 ):
         bola.setx(340)
         bola.dx *=-1
 
-    if (bola.xcor() < -340 and bola.xcor() > -350)  and (bola.ycor() < raq_1.ycor() + 60 and bola.ycor() > raq_1.ycor() - 60 
-                                                         ):
+    if (bola.xcor() < -340 and bola.xcor() > -350)  and (bola.ycor() < raq_d.ycor() + 60 and bola.ycor() > raq_d.ycor() - 60 ):
         bola.setx(-340)
         bola.dx *=-1
 
